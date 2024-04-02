543b6f38919815ad98f30a25d8002e75a9729685
Hamim K <133521471+Hamimkh@users.noreply.github.com>
2024-04-02 15:12:47 +0600

    fixed frontend

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -49,7 +49,7 @@ app.use("/api/v1/auth", authRouter);
 
 // frontend
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
